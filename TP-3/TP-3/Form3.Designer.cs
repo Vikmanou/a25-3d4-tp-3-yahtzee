a537e36ff506f8ff0e71f479c2b9849bf2e8b899
Viken Manoukian <68619882+Vikmanou@users.noreply.github.com>
2025-12-02 10:59:31 -0500

    Sauvegarde design form jeu

--- TP-3/TP-3/Form3.Designer.cs
+++ TP-3/TP-3/Form3.Designer.cs
@@ -20,125 +20,301 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+		#region Windows Form Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
-        private void InitializeComponent()
-        {
-            components = new System.ComponentModel.Container();
-            contextMenuStrip1 = new ContextMenuStrip(components);
-            nouvellePartieToolStripMenuItem = new ToolStripMenuItem();
-            label1 = new Label();
-            pictureBox1 = new PictureBox();
-            pictureBox2 = new PictureBox();
-            pictureBox3 = new PictureBox();
-            pictureBox4 = new PictureBox();
-            pictureBox5 = new PictureBox();
-            contextMenuStrip1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
-            SuspendLayout();
-            // 
-            // contextMenuStrip1
-            // 
-            contextMenuStrip1.Items.AddRange(new ToolStripItem[] { nouvellePartieToolStripMenuItem });
-            contextMenuStrip1.Name = "contextMenuStrip1";
-            contextMenuStrip1.Size = new Size(155, 26);
-            // 
-            // nouvellePartieToolStripMenuItem
-            // 
-            nouvellePartieToolStripMenuItem.Name = "nouvellePartieToolStripMenuItem";
-            nouvellePartieToolStripMenuItem.Size = new Size(154, 22);
-            nouvellePartieToolStripMenuItem.Text = "Nouvelle partie";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Font = new Font("Segoe UI", 16F);
-            label1.Location = new Point(24, 47);
-            label1.Name = "label1";
-            label1.Size = new Size(87, 30);
-            label1.TabIndex = 4;
-            label1.Text = "Yahtzee";
-            // 
-            // pictureBox1
-            // 
-            pictureBox1.Location = new Point(46, 146);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(90, 89);
-            pictureBox1.TabIndex = 5;
-            pictureBox1.TabStop = false;
-            // 
-            // pictureBox2
-            // 
-            pictureBox2.Location = new Point(162, 84);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(90, 89);
-            pictureBox2.TabIndex = 6;
-            pictureBox2.TabStop = false;
-            // 
-            // pictureBox3
-            // 
-            pictureBox3.Location = new Point(199, 215);
-            pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(90, 89);
-            pictureBox3.TabIndex = 7;
-            pictureBox3.TabStop = false;
-            // 
-            // pictureBox4
-            // 
-            pictureBox4.Location = new Point(297, 114);
-            pictureBox4.Name = "pictureBox4";
-            pictureBox4.Size = new Size(90, 89);
-            pictureBox4.TabIndex = 8;
-            pictureBox4.TabStop = false;
-            // 
-            // pictureBox5
-            // 
-            pictureBox5.Location = new Point(351, 239);
-            pictureBox5.Name = "pictureBox5";
-            pictureBox5.Size = new Size(90, 89);
-            pictureBox5.TabIndex = 9;
-            pictureBox5.TabStop = false;
-            // 
-            // Form3
-            // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(pictureBox5);
-            Controls.Add(pictureBox4);
-            Controls.Add(pictureBox3);
-            Controls.Add(pictureBox2);
-            Controls.Add(pictureBox1);
-            Controls.Add(label1);
-            Name = "Form3";
-            Text = "Form3";
-            contextMenuStrip1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
-            ResumeLayout(false);
-            PerformLayout();
-        }
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			components = new System.ComponentModel.Container();
+			contextMenuStrip1 = new ContextMenuStrip(components);
+			nouvellePartieToolStripMenuItem = new ToolStripMenuItem();
+			label1 = new Label();
+			pictureBox1 = new PictureBox();
+			label2 = new Label();
+			label3 = new Label();
+			label4 = new Label();
+			label5 = new Label();
+			label6 = new Label();
+			label7 = new Label();
+			label8 = new Label();
+			label9 = new Label();
+			label10 = new Label();
+			label11 = new Label();
+			label12 = new Label();
+			label13 = new Label();
+			label14 = new Label();
+			label15 = new Label();
+			label16 = new Label();
+			label17 = new Label();
+			label18 = new Label();
+			label19 = new Label();
+			label20 = new Label();
+			contextMenuStrip1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+			SuspendLayout();
+			// 
+			// contextMenuStrip1
+			// 
+			contextMenuStrip1.Items.AddRange(new ToolStripItem[] { nouvellePartieToolStripMenuItem });
+			contextMenuStrip1.Name = "contextMenuStrip1";
+			contextMenuStrip1.Size = new Size(155, 26);
+			// 
+			// nouvellePartieToolStripMenuItem
+			// 
+			nouvellePartieToolStripMenuItem.Name = "nouvellePartieToolStripMenuItem";
+			nouvellePartieToolStripMenuItem.Size = new Size(154, 22);
+			nouvellePartieToolStripMenuItem.Text = "Nouvelle partie";
+			// 
+			// label1
+			// 
+			label1.AutoSize = true;
+			label1.Font = new Font("Segoe UI", 16F);
+			label1.Location = new Point(24, 47);
+			label1.Name = "label1";
+			label1.Size = new Size(87, 30);
+			label1.TabIndex = 4;
+			label1.Text = "Yahtzee";
+			// 
+			// pictureBox1
+			// 
+			pictureBox1.Location = new Point(24, 323);
+			pictureBox1.Name = "pictureBox1";
+			pictureBox1.Size = new Size(90, 89);
+			pictureBox1.TabIndex = 5;
+			pictureBox1.TabStop = false;
+			// 
+			// label2
+			// 
+			label2.AutoSize = true;
+			label2.Location = new Point(633, 12);
+			label2.Name = "label2";
+			label2.Size = new Size(48, 15);
+			label2.TabIndex = 10;
+			label2.Text = "Joueur1";
+			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.Location = new Point(708, 12);
+			label3.Name = "label3";
+			label3.Size = new Size(48, 15);
+			label3.TabIndex = 11;
+			label3.Text = "Joueur1";
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Location = new Point(548, 47);
+			label4.Name = "label4";
+			label4.Size = new Size(27, 15);
+			label4.TabIndex = 12;
+			label4.Text = "Uns";
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Location = new Point(548, 145);
+			label5.Name = "label5";
+			label5.Size = new Size(37, 15);
+			label5.TabIndex = 13;
+			label5.Text = "Cinqs";
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Location = new Point(547, 120);
+			label6.Name = "label6";
+			label6.Size = new Size(48, 15);
+			label6.TabIndex = 14;
+			label6.Text = "Quatres";
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Location = new Point(549, 96);
+			label7.Name = "label7";
+			label7.Size = new Size(32, 15);
+			label7.TabIndex = 15;
+			label7.Text = "Trois";
+			// 
+			// label8
+			// 
+			label8.AutoSize = true;
+			label8.Location = new Point(548, 71);
+			label8.Name = "label8";
+			label8.Size = new Size(33, 15);
+			label8.TabIndex = 16;
+			label8.Text = "Deux";
+			// 
+			// label9
+			// 
+			label9.AutoSize = true;
+			label9.Location = new Point(549, 170);
+			label9.Name = "label9";
+			label9.Size = new Size(21, 15);
+			label9.TabIndex = 17;
+			label9.Text = "Six";
+			// 
+			// label10
+			// 
+			label10.AutoSize = true;
+			label10.Location = new Point(548, 195);
+			label10.Name = "label10";
+			label10.Size = new Size(48, 15);
+			label10.TabIndex = 18;
+			label10.Text = "Somme";
+			// 
+			// label11
+			// 
+			label11.AutoSize = true;
+			label11.Location = new Point(549, 220);
+			label11.Name = "label11";
+			label11.Size = new Size(40, 15);
+			label11.TabIndex = 19;
+			label11.Text = "Bonus";
+			// 
+			// label12
+			// 
+			label12.AutoSize = true;
+			label12.Location = new Point(548, 247);
+			label12.Name = "label12";
+			label12.Size = new Size(62, 15);
+			label12.TabIndex = 20;
+			label12.Text = "3 of a kind";
+			// 
+			// label13
+			// 
+			label13.AutoSize = true;
+			label13.Location = new Point(548, 273);
+			label13.Name = "label13";
+			label13.Size = new Size(62, 15);
+			label13.TabIndex = 21;
+			label13.Text = "4 of a kind";
+			// 
+			// label14
+			// 
+			label14.AutoSize = true;
+			label14.Location = new Point(549, 299);
+			label14.Name = "label14";
+			label14.Size = new Size(63, 15);
+			label14.TabIndex = 22;
+			label14.Text = "Full House";
+			// 
+			// label15
+			// 
+			label15.AutoSize = true;
+			label15.Location = new Point(548, 326);
+			label15.Name = "label15";
+			label15.Size = new Size(80, 15);
+			label15.TabIndex = 23;
+			label15.Text = "Small Straight";
+			// 
+			// label16
+			// 
+			label16.AutoSize = true;
+			label16.Location = new Point(548, 350);
+			label16.Name = "label16";
+			label16.Size = new Size(80, 15);
+			label16.TabIndex = 24;
+			label16.Text = "Large Straight";
+			// 
+			// label17
+			// 
+			label17.AutoSize = true;
+			label17.Location = new Point(549, 378);
+			label17.Name = "label17";
+			label17.Size = new Size(47, 15);
+			label17.TabIndex = 25;
+			label17.Text = "Chance";
+			// 
+			// label18
+			// 
+			label18.AutoSize = true;
+			label18.Location = new Point(548, 404);
+			label18.Name = "label18";
+			label18.Size = new Size(80, 15);
+			label18.TabIndex = 26;
+			label18.Text = "Large Straight";
+			// 
+			// label19
+			// 
+			label19.AutoSize = true;
+			label19.Location = new Point(549, 429);
+			label19.Name = "label19";
+			label19.Size = new Size(56, 15);
+			label19.TabIndex = 27;
+			label19.Text = "YAHTZEE";
+			// 
+			// label20
+			// 
+			label20.AutoSize = true;
+			label20.Location = new Point(549, 459);
+			label20.Name = "label20";
+			label20.Size = new Size(33, 15);
+			label20.TabIndex = 28;
+			label20.Text = "Total";
+			// 
+			// Form3
+			// 
+			AutoScaleDimensions = new SizeF(7F, 15F);
+			AutoScaleMode = AutoScaleMode.Font;
+			ClientSize = new Size(800, 497);
+			Controls.Add(label20);
+			Controls.Add(label19);
+			Controls.Add(label18);
+			Controls.Add(label17);
+			Controls.Add(label16);
+			Controls.Add(label15);
+			Controls.Add(label14);
+			Controls.Add(label13);
+			Controls.Add(label12);
+			Controls.Add(label11);
+			Controls.Add(label10);
+			Controls.Add(label9);
+			Controls.Add(label8);
+			Controls.Add(label7);
+			Controls.Add(label6);
+			Controls.Add(label5);
+			Controls.Add(label4);
+			Controls.Add(label3);
+			Controls.Add(label2);
+			Controls.Add(pictureBox1);
+			Controls.Add(label1);
+			Name = "Form3";
+			Text = "Form3";
+			contextMenuStrip1.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+			ResumeLayout(false);
+			PerformLayout();
+		}
 
-        #endregion
+		#endregion
 
-        private ContextMenuStrip contextMenuStrip1;
+		private ContextMenuStrip contextMenuStrip1;
         private ToolStripMenuItem nouvellePartieToolStripMenuItem;
         private Label label1;
         private PictureBox pictureBox1;
-        private PictureBox pictureBox2;
-        private PictureBox pictureBox3;
-        private PictureBox pictureBox4;
-        private PictureBox pictureBox5;
-    }
+		private Label label2;
+		private Label label3;
+		private Label label4;
+		private Label label5;
+		private Label label6;
+		private Label label7;
+		private Label label8;
+		private Label label9;
+		private Label label10;
+		private Label label11;
+		private Label label12;
+		private Label label13;
+		private Label label14;
+		private Label label15;
+		private Label label16;
+		private Label label17;
+		private Label label18;
+		private Label label19;
+		private Label label20;
+	}
 }
